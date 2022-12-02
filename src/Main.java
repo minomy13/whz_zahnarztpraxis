a7a609b66bec2720b01a99946c45dc970798a722
minomy13 <mail@minomy13.de>
2022-12-02 16:35:32 +0100

    angefangen mit Main Klasse

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
+import practise.Practise;
+import practise.stock.Item;
 import utils.logger.Logger;
+import utils.touple.Touple;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         Logger logger = new Logger();
+        Practise practise = new Practise(1000);
 
-        logger.info("This is a info");
-        logger.warning("This is a warning");
-        logger.error("This is a error");
-        logger.success("Succeeded");
+        ArrayList<Touple<Item, Integer>> a = new ArrayList<>();
+        a.add(new Touple(Item.COTTON_PAD, 3));
+        logger.info(practise.getBudget());
+
+        practise.getStockHandler().buy(Item.COTTON_PAD, 15, .36);
+
+        practise.getPatientHandler().getPatientFileHandler().create("Bob");
+        practise.getPatientHandler().startTreatment("A", 360.90, a);
+
+        try {
+            logger.info(practise.getStockHandler().getStock(Item.COTTON_PAD));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+
+        logger.info(practise.getBudget());
     }
 }
