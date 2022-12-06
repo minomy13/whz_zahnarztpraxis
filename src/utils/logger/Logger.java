f52936e9a61e3f46c500ddb6665468de2ea9a22f
minomy13 <mail@minomy13.de>
2022-12-06 14:54:31 +0100

    printformat statt String.format für Logger

--- src/utils/logger/Logger.java
+++ src/utils/logger/Logger.java
@@ -2,26 +2,26 @@ package utils.logger;
 
 public class Logger {
     public void info(String message) {
-        System.out.println(String.format("[Info]    %s", message));
+        System.out.printf("[Info]    %s%n", message);
     }
 
     public void info(int message) {
-        System.out.println(String.format("[Info]    %s", message));
+        System.out.printf("[Info]    %s%n", message);
     }
 
     public void info(double message) {
-        System.out.println(String.format("[Info]    %s", message));
+        System.out.printf("[Info]    %s%n", message);
     }
 
     public void warning(String message) {
-        System.out.println(String.format("\u001B[33m[Warning] %s\u001B[0m", message));
+        System.out.printf("\u001B[33m[Warning] %s\u001B[0m%n", message);
     }
 
     public void error(String message) {
-        System.out.println(String.format("\u001B[31m[Error]   %s\u001B[0m", message));
+        System.out.printf("\u001B[31m[Error]   %s\u001B[0m%n", message);
     }
 
     public void success(String message) {
-        System.out.println(String.format("\u001B[32m[Success] %s\u001B[0m", message));
+        System.out.printf("\u001B[32m[Success] %s\u001B[0m%n", message);
     }
 }
