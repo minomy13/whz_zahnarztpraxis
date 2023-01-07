0eb8806fc73f52e4ab271fc0a498ba032ec7fd58
Antonim94 <antonio.georgim94@fh-zwickau.de>
2023-01-07 17:55:02 +0100

    Employee Klassen vereinfacht

--- src/practise/employees/employeeFiles/EmployeeFileHandler.java
+++ src/practise/employees/employeeFiles/EmployeeFileHandler.java
@@ -1,10 +1,14 @@
 package practise.employees.employeeFiles;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EmployeeFileHandler {
 
     public final ArrayList<Employee> employees = new ArrayList<>();
+    public final Map<String/*Date*/,Map<String/*Name*/,Employee>> timeStampMap = new HashMap<>();
+    public final Map<String,Employee> secondMap = new HashMap<>();
 
     public void hireEmployee(String name, String job) {
         employees.add(new Employee(name, job));
@@ -20,5 +24,13 @@ public class EmployeeFileHandler {
             System.out.println(i + ": " + e.getName());
             i++;
         }
+    }
+
+    public void addEmployeesComeAndGo() {
+        employees.forEach(e -> timeStampMap.put(Calendar1.getDate(), secondMap.put(e.getName(),e)));
+    }
+
+    public String getComeAndGo(String keyDate, int keyName) {
+        return keyDate + " gekommen: " + employees.get(keyName).getCome() + " gegangen: " + employees.get(keyName).getGo();
     }
 }
