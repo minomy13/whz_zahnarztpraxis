ab709b5e73daab2eda1811dd42317017dcfb31a6
Antonim94 <antonio.georgim94@fh-zwickau.de>
2023-01-10 16:11:34 +0100

    123

--- src/practise/Practise.java
+++ src/practise/Practise.java
@@ -31,7 +31,7 @@ public class Practise {
 
         // creates a new instance for every handler
         this.calendar = new Calendar1(year, month, dayOfMonth, hour, minute);
-        this.employeeHandler = new EmployeeFileHandler();
+        this.employeeHandler = new EmployeeHandler();
         this.patientHandler = new PatientHandler(this);
         this.stockHandler = new StockHandler(this);
         //this.clock = new Clock(hour,minute,1);
@@ -67,7 +67,7 @@ public class Practise {
         return calendar;
     }
 
-    public EmployeeFileHandler getEmployeeHandler() {
+    public EmployeeHandler getEmployeeHandler() {
         return employeeHandler;
     }
 
