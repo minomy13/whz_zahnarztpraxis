fcacbd80540f088f7313973394eb5a00c1e6f4f1
Lisette <Lisette.Kratkey@gmx.de>
2023-01-15 23:23:37 +0100

    10th day

--- src/Main.java
+++ src/Main.java
@@ -112,7 +112,7 @@ public class Main {
         System.out.println("\n --- \n");
 
         logger.info("Creating new patient files...");
-        logger.info("Creating patient \"Bob\", born 1996...");
+        logger.info("Creating patient file for \"Bob\", born in 1996...");
         practise.getPatientHandler().getPatientFileHandler().create("Bob", 1996);
         try {
             practise.getPatientHandler().getPatientFileHandler().getFileByNameAndBirth("Bob", 1996)
@@ -122,7 +122,7 @@ public class Main {
             logger.error(e.getMessage());
         }
 
-        logger.info("Creating patient \"Max\", born 2001...");
+        logger.info("Creating patient file for \"Max\", born in 2001...");
         practise.getPatientHandler().getPatientFileHandler().create("Max", 2001);
         try {
             practise.getPatientHandler().getPatientFileHandler().getFileByNameAndBirth("Max", 2001)
@@ -132,7 +132,7 @@ public class Main {
             logger.error(e.getMessage());
         }
 
-        logger.info("Creating patient \"Bob\", born 1987...");
+        logger.info("Creating patient file for \"Bob\", born in 1987...");
         practise.getPatientHandler().getPatientFileHandler().create("Bob", 1987);
         try {
             practise.getPatientHandler().getPatientFileHandler().getFileByNameAndBirth("Bob", 1987)
@@ -145,14 +145,14 @@ public class Main {
         System.out.println("\n --- \n");
 
         try {
-            logger.info("Amount of moth mirrors in stock is " + practise.getStockHandler().getStock(Item.MOUTH_MIRROR));
+            logger.info("Amount of mouth mirrors in stock is " + practise.getStockHandler().getStock(Item.MOUTH_MIRROR));
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
         logger.info("Taking a mouth mirror from stock...");
         try {
             practise.getStockHandler().take(Item.MOUTH_MIRROR, 1);
-            logger.success("Success! Amount of moth mirrors in stock is now " +
+            logger.success("Success! Amount of mouth mirrors in stock is now " +
                     practise.getStockHandler().getStock(Item.MOUTH_MIRROR));
         } catch (Exception e) {
             logger.error(e.getMessage());
@@ -188,6 +188,7 @@ public class Main {
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
+        practise.getCalendar().getCurrentTime();
 
         logger.success("Appointments done!");
 
@@ -223,5 +224,181 @@ public class Main {
         practise.getEmployeeHandler().getComeAndGoAll("1.0.2022");
         System.out.println();
         practise.getEmployeeHandler().getWorkTime();
+
+        System.out.println("\n --- \n");
+        try {
+            practise.advanceTime(8, 16, 00);
+        } catch(Exception e ) {
+            logger.error(e.getMessage());
+        }
+        System.out.println("Skip to: " + practise.getCalendar().getTime());
+
+        logger.info("Hiring new employee...");
+        practise.getEmployeeHandler().hireEmployee("Paul", "dental hygenienist");
+        practise.getEmployeeHandler().viewEmployees();
+
+        System.out.println("\n --- \n");
+        System.out.println("\n --- \n");
+
+        try {
+            practise.advanceTime(0, 0, 10);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        practise.getEmployeeHandler().employeeCome(0);
+        try {
+        practise.advanceTime(0, 0, 20);
+    } catch (Exception e) {
+        logger.error(e.getMessage());
     }
+        practise.getEmployeeHandler().employeeCome(1);
+        try {
+        practise.advanceTime(0, 0, 30);
+    } catch (Exception e) {
+        logger.error(e.getMessage());
+    }
+        practise.getEmployeeHandler().employeeCome(2);
+        try {
+            practise.advanceTime(0, 0, 30);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        practise.getEmployeeHandler().employeeCome(3);
+
+        logger.info("Employees arriving...\n" + practise.getEmployeeHandler().getEmployeeName(0) + " " + practise.getEmployeeHandler().getEmployeeCome(0) + "\n" +
+            practise.getEmployeeHandler().getEmployeeName(1) + " " + practise.getEmployeeHandler().getEmployeeCome(1) + "\n" +
+            practise.getEmployeeHandler().getEmployeeName(2) + " " + practise.getEmployeeHandler().getEmployeeCome(2) + "\n" +
+            practise.getEmployeeHandler().getEmployeeName(3) + " " + practise.getEmployeeHandler().getEmployeeCome(3));
+
+        System.out.println("\n --- \n");
+
+        logger.info("Buying new stock...");
+        practise.getStockHandler().buy(Item.PLASTIC_CUP, 27, 0.02);
+        practise.getStockHandler().buy(Item.COTTON_PAD, 5, 0.05);
+        practise.getStockHandler().buy(Item.MOUTH_MIRROR, 12, 5.99);
+        practise.getStockHandler().buy(Item.DENTAL_SYRINGE, 6, 1.3);
+        practise.getStockHandler().buy(Item.DENTAL_PROBE, 19, 4.03);
+        practise.getStockHandler().buy(Item.ANESTHETIC, 14, 23);
+        practise.getStockHandler().buy(Item.WHITENING_STRIPS, 5, 11);
+
+        try {
+        logger.info("Stock is now: " + practise.getStockHandler().getAllStocks());
+    } catch (Exception e) {
+        logger.error(e.getMessage());
+    }
+
+        System.out.println("\n --- \n");
+
+        practise.getPatientHandler().viewTreatmentTypes();
+        System.out.println();
+        practise.viewRoomHandler();
+
+        System.out.println("\n --- \n");
+
+        logger.info("Creating new patient file...");
+        logger.info("Creating patient file for \"Kathrine\", born in 1965...");
+        practise.getPatientHandler().getPatientFileHandler().create("Kathrine", 1965);
+        try {
+        practise.getPatientHandler().getPatientFileHandler().getFileByNameAndBirth("Kathrine", 1965)
+                .createReport("Due for a Check-Up", practise.getPatientHandler().getTreatmentTypeIndex("Check-Up"));
+        logger.success("Success!");
+    } catch (Exception e) {
+        logger.error(e.getMessage());
+    }
+
+        try {
+        practise.getPatientHandler().getPatientFileHandler().getFileByNameAndBirth("Bob", 1996)
+                .createReport("Has to have a tooth removed", practise.getPatientHandler().getTreatmentTypeIndex("Removal"));
+        logger.success("Success!");
+    } catch (Exception e) {
+        logger.error(e.getMessage());
+    }
+
+        System.out.println("\n --- \n");
+
+        try {
+        logger.info("Amount of Cotton Pad in stock is " + practise.getStockHandler().getStock(Item.COTTON_PAD));
+    } catch (Exception e) {
+        logger.error(e.getMessage());
+    } //TODO taken items and stock
+        logger.info("Taking a mouth mirror from stock...");
+        try {
+        practise.getStockHandler().take(Item.MOUTH_MIRROR, 1);
+        logger.success("Success! Amount of mouth mirrors in stock is now " +
+                practise.getStockHandler().getStock(Item.MOUTH_MIRROR));
+    } catch (Exception e) {
+        logger.error(e.getMessage());
+    }
+
+        System.out.println("\n --- \n");
+
+        logger.info("Adding appointments to calendar...");
+        try {
+        practise.getRoomHandler().get(0).getAppointmentCalendar().addAppointment(2022, 0, 10, 12, 10, 12, 30,
+                practise.getPatientHandler().getPatientFileHandler().getFileByNameAndBirth("Kathrine", 1965));
+    } catch (Exception e) {
+        logger.error(e.getMessage());
+    }
+        try {
+        practise.getRoomHandler().get(0).getAppointmentCalendar().addAppointment(2022, 0, 10, 13, 30, 14, 10,
+                practise.getPatientHandler().getPatientFileHandler().getFileByNameAndBirth("Bob", 1996));
+    } catch (Exception e) {
+        logger.error(e.getMessage());
+    }
+
+        if (practise.getRoomHandler().get(0).getAppointmentCalendar().getAppointmentMap().size() == 2)
+            logger.success("Success!");
+
+        System.out.println("\n --> advance time by 8h --> \n");
+        try {
+        practise.advanceTime(0, 8, 0);
+    } catch (Exception e) {
+        logger.error(e.getMessage());
+    }
+        practise.getCalendar().getCurrentTime();
+
+        logger.success("Appointments done!");
+
+        System.out.println("\n --- \n");
+
+        logger.info("Employees going home...");
+        practise.getEmployeeHandler().employeeGo(3);
+        logger.success(String.format("Employee %s went home", practise.getEmployeeHandler().getEmployeeName(3)));
+
+        System.out.println("\n --> advance time by 30min --> \n");
+        try {
+            practise.advanceTime(0, 0, 30);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        practise.getEmployeeHandler().employeeGo(2);
+        logger.success(String.format("Employee %s went home", practise.getEmployeeHandler().getEmployeeName(2)));
+
+        System.out.println("\n --> advance time by 30min --> \n");
+        try {
+        practise.advanceTime(0, 0, 30);
+    } catch (Exception e) {
+        logger.error(e.getMessage());
+    }
+        practise.getEmployeeHandler().employeeGo(1);
+        logger.success(String.format("Employee %s went home", practise.getEmployeeHandler().getEmployeeName(1)));
+
+        System.out.println("\n --> advance time by 30min --> \n");
+        try {
+        practise.advanceTime(0, 0, 30);
+    } catch (Exception e) {
+        logger.error(e.getMessage());
+    }
+        practise.getEmployeeHandler().employeeGo(0);
+        logger.success(String.format("Employee %s went home", practise.getEmployeeHandler().getEmployeeName(0)));
+
+        System.out.println("\n --- \n");
+
+        logger.info("Summary of day 10:");
+        practise.getEmployeeHandler().addEmployeeTimeStamp();
+    //System.out.println(practise.getEmployeeHandler().getComeAndGo("10.0.2022", practise.getEmployeeHandler().getEmployeeName(0)));
+        practise.getEmployeeHandler().getComeAndGoAll("10.0.2022");
+        System.out.println();
+        practise.getEmployeeHandler().getWorkTime();
+}
 }
